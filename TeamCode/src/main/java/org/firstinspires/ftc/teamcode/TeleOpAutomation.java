d74bc6badc56a0422342b5f82ae4d720f4801f5b
FTCTe <ftcteam23493@gmail.com>
2025-09-10 20:41:45 -0400

    Add PIDF Code and Drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpAutomation.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.PIDFController;
+import org.firstinspires.ftc.teamcode.PIDFParams;
+import org.firstinspires.ftc.teamcode.Drive;
+
+@TeleOp(name = "TeleOp", group = "TeleOp")
+public class TeleOpAutomation {
+}
