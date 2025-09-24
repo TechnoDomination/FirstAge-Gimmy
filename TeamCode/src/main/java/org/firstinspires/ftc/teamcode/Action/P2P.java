8083af02745a307e07d3ac2811200c54b78269d5
FTCTe <ftcteam23493@gmail.com>
2025-09-24 19:10:35 -0400

    Fixed gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Action/P2P.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.Action;
-
-public class P2P {
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBuildaPinPointOdo/PinpointTest.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.GoBuildaPinPointOdo;
-
-public class PinpointTest {
-}
