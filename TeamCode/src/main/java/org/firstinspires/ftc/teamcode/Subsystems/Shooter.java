22f5f5b964941d5375009a8f8ecb6a87c1ca899f
FTCTe <ftcteam23493@gmail.com>
2025-10-01 20:14:49 -0400

    SHOOTER START

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.PIDF.PIDFController;
+import org.firstinspires.ftc.teamcode.PIDF.PIDFParams;
+
 public class Shooter {
+
+    public static Drive instance;
+    public DcMotorEx ShooterMotor;
+
+    public Shooter (HardwareMap hardwareMap){
+        ShooterMotor = hardwareMap.get(DcMotorEx.class, "FrontLeftDCMotor");
+    }
 }
