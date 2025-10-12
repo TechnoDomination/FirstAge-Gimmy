117b3e190bbdef0f9596477e132f4c61a988e749
Dummy-Avika <avikalakhotia2012@gmail.com>
2025-10-12 19:03:39 -0400

    Hopper code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpAutomation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpAutomation.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Subsystems.Drive;
+import org.firstinspires.ftc.teamcode.Subsystems.Hopper;
 import org.firstinspires.ftc.teamcode.Subsystems.Shooter;
 
 @TeleOp(name = "TeleOp", group = "TeleOp")
@@ -14,6 +15,7 @@ public class TeleOpAutomation extends LinearOpMode {
         Shooter shooter = new Shooter(hardwareMap);
         waitForStart();
         drive.update(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        Hopper hopper = new Hopper(hardwareMap);
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Hopper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Hopper.java
@@ -1,6 +1,22 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Hopper {
 
     public static Hopper instance;
+    public DcMotorEx HopperMotor;
+
+    public Hopper (HardwareMap hardwareMap){
+
+        HopperMotor = hardwareMap.get(DcMotorEx.class, "FrontLeftDcMotor");
+        HopperMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        HopperMotor.setPower(0);
+        HopperMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        instance = this;
+    }
+
+
 }
