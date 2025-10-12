d8d78b9391a289671dc004609c7fe80cc3926313
FTCTe <ftcteam23493@gmail.com>
2025-10-12 19:03:42 -0400

    AUTO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/AutoStartBlueGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/AutoStartBlueGoal.java
@@ -1,12 +1,29 @@
 package org.firstinspires.ftc.teamcode.OpModes.Auto;
 
+import static java.lang.Math.PI;
+
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.GoBildaPinPointOdo.Localizer;
+import org.firstinspires.ftc.teamcode.GoBildaPinPointOdo.Poses;
+import org.firstinspires.ftc.teamcode.Positions;
+import org.firstinspires.ftc.teamcode.Subsystems.Drive;
 
-@Autonomous(name = "AutoBlue")
+@Autonomous(name = "AutoBlue", group = "Auto")
 public class AutoStartBlueGoal extends LinearOpMode {
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
 
+        Localizer localizer = new Localizer(hardwareMap, new Poses(-47.4, 56.3, -PI * 0.95));
+        Drive drive = new Drive(hardwareMap);
+
+        new SequentialAction(
+                Positions.MoveOutTriangleBlueGoal.runToExact,
+                new SleepAction(0.5),
+                Positions.ShootingPositionsBlue.runToExact,
+                new SleepAction(0.5)
+        );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/AutoStartRedGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/AutoStartRedGoal.java
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode.OpModes.Auto;
 
-public class AutoStartRedGoal {
+import static java.lang.Math.PI;
+
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.GoBildaPinPointOdo.Localizer;
+import org.firstinspires.ftc.teamcode.GoBildaPinPointOdo.Poses;
+import org.firstinspires.ftc.teamcode.Positions;
+import org.firstinspires.ftc.teamcode.Subsystems.Drive;
+
+@Autonomous(name = "AutoRed", group = "Auto")
+public class AutoStartRedGoal extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+        Localizer localizer = new Localizer(hardwareMap, new Poses(48,55.7,PI*0.95));
+        Drive drive = new Drive(hardwareMap);
+
+        new SequentialAction(
+                Positions.MoveOutTriangleRedGoal.runToExact,
+                new SleepAction(0.5),
+                Positions.ShootingPositionRed.runToExact,
+                new SleepAction(0.5)
+        );
+    }
 }
