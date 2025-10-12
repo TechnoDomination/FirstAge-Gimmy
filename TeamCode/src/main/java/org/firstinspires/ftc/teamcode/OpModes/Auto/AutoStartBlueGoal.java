3a69249d9647fb49c8bfa62889209a166cc4af36
FTCTe <ftcteam23493@gmail.com>
2025-10-12 18:12:27 -0400

    Auto Positions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/AutoStartBlueGoal.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.OpModes.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "AutoBlue")
+public class AutoStartBlueGoal extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Hopper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Hopper.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
 public class Hopper {
+
+    public static Hopper instance;
 }
