9d0cbf5dc9eb9dc88baad7ca523ade6024377e62
FTCTe <ftcteam23493@gmail.com>
2025-10-12 20:06:48 -0400

    TeleOp additions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpAutomation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpAutomation.java
@@ -13,9 +13,23 @@ public class TeleOpAutomation extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Drive drive = new Drive(hardwareMap);
         Shooter shooter = new Shooter(hardwareMap);
+        Hopper hopper = new Hopper(hardwareMap);
         waitForStart();
         drive.update(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-        Hopper hopper = new Hopper(hardwareMap);
+
+        //shooter
+        if (gamepad1.right_bumper) {
+            while (gamepad1.right_bumper) {
+             shooter.ShooterMotor.setPower(0.85);
+            }
+        }
+
+        //hopper
+        if (gamepad1.left_bumper) {
+            while (gamepad1.left_bumper) {
+                hopper.HopperMotor.setPower(0.85);
+            }
+        }
 
     }
 }
