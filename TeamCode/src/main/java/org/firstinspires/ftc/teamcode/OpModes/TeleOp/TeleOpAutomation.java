44d36a340222dc07bffaa37d54468df08624e50e
FTCTe <ftcteam23493@gmail.com>
2025-10-29 19:13:34 -0400

    Shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpAutomation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpAutomation.java
@@ -1,51 +1,77 @@
 package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 
 import org.firstinspires.ftc.teamcode.Subsystems.Drive;
 import org.firstinspires.ftc.teamcode.Subsystems.Hopper;
 import org.firstinspires.ftc.teamcode.Subsystems.Shooter;
 
+
 @TeleOp(name = "TeleOp", group = "TeleOp")
 public class TeleOpAutomation extends LinearOpMode {
 
+
+    double motorMaxRPM = 6000;
+    double ticksPerRev = 28;
+    double desiredRPM;
+    double velocityB = 0;
+    double velocityA;
+    double velocityY;
+    double velocityX;
+
+
     @Override
     public void runOpMode() {
+
 
         Drive drive = new Drive(hardwareMap);
         Shooter shooter = new Shooter(hardwareMap);
         Hopper hopper = new Hopper(hardwareMap);
 
+
         shooter.ShooterMotor.setPower(0.0);
+
 
         waitForStart();
         while (opModeIsActive() && !isStopRequested()) {
             hopper.update();
             telemetry.update();
 
+
             //drive
             drive.update(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
 
+
             //shooter
             if (gamepad1.a) {
-                    shooter.ShooterMotor.setPower(0);
+                shooter.ShooterMotor.setPower(0); //setPower(0)
             }
             if (gamepad1.b) {
-                    shooter.ShooterMotor.setPower(0.47);
+
+
+                shooter.setVelocityRPM(2000);//setPower(0.47)
+
+
+
 
             }
             if (gamepad1.y) {
-                    shooter.ShooterMotor.setPower(0.57);
-
+                shooter.setVelocityRPM(3500);
             }
             if (gamepad1.x) {
 
-                    shooter.ShooterMotor.setPower(0.7);
+
+                shooter.ShooterMotor.setVelocity(velocityX); //setPower(0.7)
+                desiredRPM = 4200;
+                velocityX = (desiredRPM / 60.0) * ticksPerRev;
             }
 
-            telemetry.addData("Shooter Power:", shooter.ShooterMotor.getPower());
 
+            telemetry.addData("Shooter Power:", shooter.ShooterMotor.getVelocity());
+            telemetry.update();
             //hopper
             if (gamepad1.right_bumper) {
                 hopper.state = Hopper.State.UP;
@@ -53,6 +79,7 @@ public class TeleOpAutomation extends LinearOpMode {
             if (gamepad1.left_bumper) {
                 hopper.state = Hopper.State.DOWN;
             }
+
 
             telemetry.addData("Hopper: ", hopper.getHopperTelemetry());
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
@@ -1,17 +1,22 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+
 import org.firstinspires.ftc.teamcode.PIDF.PIDFController;
 import org.firstinspires.ftc.teamcode.PIDF.PIDFParams;
 
+
 public class Shooter {
+
 
     public static Shooter instance;
     public DcMotorEx ShooterMotor;
+
 
     public Shooter (HardwareMap hardwareMap){
         ShooterMotor = hardwareMap.get(DcMotorEx.class, "ShooterMotor");
@@ -21,18 +26,22 @@ public class Shooter {
         instance = this;
     }
 
+
     public void setVelocityRPM(double targetRPM) {
         // Prevent setting a velocity above the motor's capability.
+
 
         // Convert RPM to ticks per second.
         double targetVelocityTPS = (targetRPM / 60) * 28;
         ShooterMotor.setVelocity(targetVelocityTPS);
 
+
     }
 
+
     public void zero(){
+
 
         ShooterMotor.setPower(0);
     }
 }
-
