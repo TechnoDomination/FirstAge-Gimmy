ff6a856fa30452ba86102f1301c49a6d3854e0a5
FTCTe <ftcteam23493@gmail.com>
2025-11-05 19:50:38 -0500

    AUTO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Positions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Positions.java
@@ -13,12 +13,17 @@ public enum Positions {
     MoveOutTriangleRedGoal(new Vector2d(40, 15), PI*.25),
     ShootingPositionsRed(new Vector2d(35, 35), PI*0.25),
     ParkPositionsRed(new Vector2d(60, 25), PI*0.25),
+    TurningRed(new Vector2d(0,  80), PI*0.25),
+    EndingRed(new Vector2d(20,  60), PI*0.25),
 
     //Auto Blue Goal - LOCALIZER = x:-47.4, y:56.3, rotation:-PI*0.95
     MoveRightBlueGoal(new Vector2d(-13 , 55.8), 0.0),
     ShootingPositionsBlue(new Vector2d(-35, 15), PI*-0.25),
     ParkPositionsBlue(new Vector2d(-60, 15), PI*-0.25),
-    MoveForward(new Vector2d(0,  10), 0.0);
+    MoveForward(new Vector2d(0,  50), 0.0),
+    TurningBlue(new Vector2d(0,  80), PI*-0.25),
+    EndingBlue(new Vector2d(-20,  60), PI*-0.25);
+
 
     Positions(Vector2d vector, Double rotation) {
         runToExact = new P2P(vector, rotation);
